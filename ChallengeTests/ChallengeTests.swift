3dc64be6365aea7109d481d0e8b9a6651cda4d67
Nah <nahung89@gmail.com>
2019-02-17 01:13:24 +0700

    1st source codes

--- ChallengeTests/ChallengeTests.swift
+++ ChallengeTests/ChallengeTests.swift
@@ -6,29 +6,30 @@
 //  Copyright © 2019 Nah. All rights reserved.
 //
 
-import XCTest
 @testable import Challenge
+import XCTest
 
 class ChallengeTests: XCTestCase {
-
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("test")
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- ChallengeUITests/ChallengeUITests.swift
+++ ChallengeUITests/ChallengeUITests.swift
@@ -9,8 +9,8 @@
 import XCTest
 
 class ChallengeUITests: XCTestCase {
-
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -23,12 +23,13 @@ class ChallengeUITests: XCTestCase {
     }
 
     override func tearDown() {
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("test")
     }
-
 }
